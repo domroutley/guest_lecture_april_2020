36497442ad95cbda45eeb5f3a2b7878280ca40bd
domroutley <dominic.routley@gmail.com>
2019-10-01 11:41:57 +0100

    added ip config to network interface and added subnet

--- networking.tf
+++ networking.tf
@@ -9,6 +9,20 @@ resource "azurerm_network_interface" "agent" {
   name                = "${var.prefix}-network_interface"
   resource_group_name = "${azurerm_resource_group.agent.name}"
   location            = "${var.location}"
+  ip_configuration {
+    name                          = "${var.prefix}-address"
+    public_ip_address_id          = "${azurerm_public_ip.agent.id}"
+    private_ip_address_version    = "IPv4"
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = "${azurerm_subnet.agent.id}"
+  }
+}
+
+resource "azurerm_subnet" "agent" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = "${azurerm_resource_group.agent.name}"
+  virtual_network_name = "${azurerm_virtual_network.agent.name}"
+  address_prefix       = "10.0.1.0/24"
 }
 
 resource "azurerm_public_ip" "agent" {
