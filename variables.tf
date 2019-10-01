1ade7a53f7ed1559ec94813bc47507cfcdf74119
domroutley <dominic.routley@gmail.com>
2019-10-01 11:42:31 +0100

    added username and password empty variables

--- variables.tf
+++ variables.tf
@@ -9,3 +9,8 @@ variable "location" {
 variable "vm_size" {
   default = "Standard_B2s"
 }
+
+# Declared but filled from a secret file (secrets.auto.tfvars)
+# so as not to expose secrets to source control
+variable "os_user" {}
+variable "os_pass" {}
