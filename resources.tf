85972650b2f5e81fdeadad7010e864462af97f36
domroutley <dominic.routley@gmail.com>
2019-10-01 11:43:05 +0100

    fixed bug with wrongly named resource group

--- resources.tf
+++ resources.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "agent" {
 
 resource "azurerm_virtual_machine" "agent" {
   name                      = "${var.prefix}-virtual_machine"
-  resource_group_name       = "${azurerm_resource_group.main.name}"
+  resource_group_name       = "${azurerm_resource_group.agent.name}"
   location                  = "${var.location}"
   network_interface_ids     = ["${azurerm_network_interface.agent.id}"]
   vm_size                   = "${var.vm_size}"
