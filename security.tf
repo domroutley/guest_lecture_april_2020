5cb58fa4284c7b317d07e9fa82a355a253b04601
domroutley <dominic.routley@gmail.com>
2019-10-02 16:23:31 +0100

    add RDP access rule

--- security.tf
+++ security.tf
@@ -3,3 +3,18 @@ resource "azurerm_network_security_group" "agent" {
   name                = "${var.prefix}-network_security_group"
   location            = "${var.location}"
 }
+
+resource "azurerm_network_security_rule" "agent" {
+  resource_group_name         = "${azurerm_resource_group.agent.name}"
+  network_security_group_name = "${azurerm_network_security_group.agent.name}"
+  name                        = "Ingress-RDP_NSG-${var.prefix}"
+  description                 = "Allow RDP access"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+}
