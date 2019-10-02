a6641f1040b513bf8966c95933e77bddb20fcab4
domroutley <dominic.routley@gmail.com>
2019-10-02 15:10:15 +0100

    added service principal credentials variables

--- provider.tf
+++ provider.tf
@@ -1,3 +1,7 @@
 provider "azurerm" {
   version = "=1.34.0"
+  subscription_id = "${var.subscription_id}"
+  tenant_id       = "${var.tenant_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
 }
--- variables.tf
+++ variables.tf
@@ -10,6 +10,13 @@ variable "vm_size" {
   default = "Standard_B2s"
 }
 
+# Service Principal credentials
+variable "subscription_id" {}
+variable "tenant_id" {}
+
+variable "client_id" {}
+variable "client_secret" {}
+
 # Declared but filled from a secret file (secrets.auto.tfvars)
 # so as not to expose secrets to source control
 variable "os_user" {}
