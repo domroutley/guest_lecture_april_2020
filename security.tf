7838733b2b773e9de4467b7ff43389470cdffb7f
domroutley <dominic.routley@gmail.com>
2019-10-07 09:04:51 +0100

    added ssh rule

--- security.tf
+++ security.tf
@@ -18,3 +18,18 @@ resource "azurerm_network_security_rule" "http_in" {
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
 }
+
+resource "azurerm_network_security_rule" "ssh_in" {
+  resource_group_name         = "${azurerm_resource_group.agent.name}"
+  network_security_group_name = "${azurerm_network_security_group.agent.name}"
+  name                        = "Ingress-SSH_NSG-${var.prefix}"
+  description                 = "Allow SSH access"
+  priority                    = 111
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+}
