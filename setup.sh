2af2554ab3f44b4f8d3f8ce603b43e096f1cf305
domroutley <dominic.routley@gmail.com>
2019-11-12 13:28:24 +0000

    reverted to bash hello world

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-wget 'https://gist.githubusercontent.com/domroutley/5e26e2d13000f4003ba6fddb13e23d0c/raw/e8b5c142136745c180fe9b800f14c61a2bc3e634/server.py'
-mv server.py $HOME/server.py
-python3 $HOME/server.py 8080 # TODO, turn this into a service
+echo "Hello world" > index.html
+nohup busybox httpd -f -p "8080" &
